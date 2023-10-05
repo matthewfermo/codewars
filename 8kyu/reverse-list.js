af8c5bc525e171917cf43b923325cea527358f76
Matthew Fermo <98434804+matthewfermo@users.noreply.github.com>
2023-10-05 09:34:36 -0400

    reverse-list 8kyu

---
+++ 8kyu/reverse-list.js
@@ -0,0 +1,8 @@
+// instruction
+// In this kata you will create a function that takes in a list and returns a list with the reverse order.
+
+//solution
+
+function reverseList(list) {
+  return list.reverse();
+}
