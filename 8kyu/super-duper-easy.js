7a321eb01049a89db149118c64afd2d5c9775889
Matthew Fermo <98434804+matthewfermo@users.noreply.github.com>
2023-10-06 13:52:06 -0400

    super-duper-easy.js added

---
+++ 8kyu/super-duper-easy.js
@@ -0,0 +1,6 @@
+// DESCRIPTION:
+// Make a function that returns the value multiplied by 50 and increased by 6. If the value entered is a string it should return "Error".
+
+//solution
+
+problem = (x) => (typeof x == "number" ? x * 50 + 6 : "Error");
