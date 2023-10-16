435f5c4b21398a1116caca5a5b7c1dc491556762
Matthew Fermo <98434804+matthewfermo@users.noreply.github.com>
2023-10-16 15:31:33 -0400

    adding sum of positive 8kyu codewar

---
+++ 8kyu/sum-of-positive.js
@@ -0,0 +1,14 @@
+//instructions
+
+// You get an array of numbers, return the sum of all of the positives ones.
+
+// Example [1,-4,7,12] => 1 + 7 + 12 = 20
+
+// Note: if there is nothing to sum, the sum is default to 0.
+
+//solution
+function positiveSum(arr) {
+  const reducer = (accumulator, current) =>
+    accumulator + (current > 0 ? current : 0);
+  return arr.reduce(reducer, 0);
+}
